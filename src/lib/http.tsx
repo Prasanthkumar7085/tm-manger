edd7564e4d6a133b78f42062ff871536b580bb64
code@labsquire.com <code@labsquire.com>
2024-10-20 16:13:17 +0530

    updated changes in the api

--- src/lib/http.tsx
+++ src/lib/http.tsx
@@ -1,26 +1,25 @@
-
-const API_BASE_URL = "https://dev-api-ashikagroup.workr.in/v1.0";//"http://localhost:3000/v1.0"
+const API_BASE_URL = "https://api.example.com";
 
 export const getData = async (urlPath: string) => {
   const response = await fetch(`${API_BASE_URL}/${urlPath}`);
   const result = await response.json();
   if (!response.ok) {
-    throw new Error(result.message || 'Unknown Error')
+    throw new Error(result.message || "Unknown Error");
   }
-  return result
-}
+  return result;
+};
 
 export const postData = async (urlPath: string, data: any) => {
   const response = await fetch(`${API_BASE_URL}/${urlPath}`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   });
   const result = await response.json();
   if (!response.ok) {
-    throw new Error(result.message || 'Unknown Error')
+    throw new Error(result.message || "Unknown Error");
   }
-  return result
-}
+  return result;
+};
