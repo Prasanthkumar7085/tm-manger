4c09be18edaf90f3782a1c7b4cb02f6ce44016fd
Manikanta M <manikanta.m@labsquire.com>
2024-10-28 19:28:37 +0530

    Users List UI

--- src/components/Users/index.tsx
+++ src/components/Users/index.tsx
@@ -476,10 +476,10 @@ function UsersTable() {
                 </li>
                 <li>
                   <Button
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm  ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-primary/90 py-2 bg-red-700 text-white h-[35px] px-6 font-semibold"
                     onClick={() => handleDrawerOpen()}
                   >
-                    +Add Users
+                    + Add Users
                   </Button>
                 </li>
                 {/* <li>
