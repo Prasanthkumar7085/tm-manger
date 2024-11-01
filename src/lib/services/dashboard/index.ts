41c0c42a77a8952c3a792e4375ca55b71ba331d6
code <code@labsquire>
2024-11-01 19:16:26 +0530

    MA--added date filter in stats api

--- src/lib/services/dashboard/index.ts
+++ src/lib/services/dashboard/index.ts
@@ -1,30 +1,30 @@
 import { $fetch } from "@/lib/fetch";
 
-export const getTotalProjectsStats = async () => {
+export const getTotalProjectsStats = async (queryParams: any) => {
   try {
-    return await $fetch.get(`/stats/total-projects`);
+    return await $fetch.get(`/stats/total-projects`, queryParams);
   } catch (err: any) {
     throw err;
   }
 };
 
-export const getTotalUsersStats = async () => {
+export const getTotalUsersStats = async (queryParams: any) => {
   try {
-    return await $fetch.get(`/stats/total-users`);
+    return await $fetch.get(`/stats/total-users`, queryParams);
   } catch (err: any) {
     throw err;
   }
 };
-export const getTotalTasksStats = async () => {
+export const getTotalTasksStats = async (queryParams: any) => {
   try {
-    return await $fetch.get(`/stats/total-tasks`);
+    return await $fetch.get(`/stats/total-tasks`, queryParams);
   } catch (err: any) {
     throw err;
   }
 };
-export const getTotalActiveStats = async () => {
+export const getTotalActiveStats = async (queryParams: any) => {
   try {
-    return await $fetch.get(`/stats/total-active-tasks`);
+    return await $fetch.get(`/stats/total-active-tasks`, queryParams);
   } catch (err: any) {
     throw err;
   }
