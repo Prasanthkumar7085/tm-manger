8d68f4c581bf4826cb68ec17d02bbcaf05532a3a
code@labsquire.com <code@labsquire.com>
2024-11-01 12:18:24 +0530

    Pr-update tags component

--- src/components/Tasks/Add/index.tsx
+++ src/components/Tasks/Add/index.tsx
@@ -28,6 +28,7 @@ import { Check, ChevronsUpDown, X } from "lucide-react";
 import React, { useState } from "react";
 import { DatePicker } from "rsuite";
 import { toast } from "sonner";
+import TagsComponent from "./TagsComponent";
 
 const AddTask = () => {
   const navigate = useNavigate();
@@ -57,33 +58,6 @@ const AddTask = () => {
   const [selectedUsers, setSelectedUsers] = useState(new Set());
   const handleChange = (e: any) => {
     setTask({ ...task, [e.target.name]: e.target.value });
-  };
-
-  const handleTagSubmit = () => {
-    if (tagInput.trim() && !task.tags.includes(tagInput.trim())) {
-      setErrorMessages((prev: any) => ({
-        ...prev,
-        tags: [""],
-      }));
-      setTask((prev: any) => ({
-        ...prev,
-        tags: [...prev.tags, tagInput.trim()],
-      }));
-      setTagInput("");
-    }
-    if (task.tags.includes(tagInput.trim())) {
-      setErrorMessages((prev: any) => ({
-        ...prev,
-        tags: ["Tag already exists"],
-      }));
-    }
-  };
-
-  const handleTagDelete = (tag: any) => {
-    setTask((prev: any) => ({
-      ...prev,
-      tags: prev.tags.filter((t: any) => t !== tag),
-    }));
   };
 
   const handleProjectSelect = (project: any) => {
@@ -494,52 +468,15 @@ const AddTask = () => {
                   )}
                 </div>
 
-                <div className="mb-4">
-                  <label className="block text-gray-700 font-bold mb-2">
-                    Tags
-                  </label>
-                  <div className="flex">
-                    <input
-                      type="text"
-                      value={tagInput}
-                      onChange={(e) => setTagInput(e.target.value)}
-                      onKeyDown={(e) => {
-                        if (e.key === "Enter") {
-                          handleTagSubmit();
-                          e.preventDefault();
-                        }
-                      }}
-                      className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
-                      placeholder="Enter tag"
-                    />
-                    <Button
-                      type="button"
-                      onClick={handleTagSubmit}
-                      className="ml-2"
-                    >
-                      Add
-                    </Button>
-                  </div>
-                  {errorMessages.tags && (
-                    <p style={{ color: "red" }}>{errorMessages?.tags?.[0]}</p>
-                  )}
-                  <div className="flex flex-wrap mt-2">
-                    {task.tags.map((tag: any, index: number) => (
-                      <div
-                        key={index}
-                        className="flex items-center mt-2 px-3 py-1 bg-green-100 text-green-800 text-sm rounded mr-2"
-                      >
-                        {tag}
-                        <p
-                          className="ml-1 text-red-500 rotate-[45deg] cursor-pointer"
-                          onClick={() => handleTagDelete(tag)}
-                        >
-                          +
-                        </p>
-                      </div>
-                    ))}
-                  </div>
-                </div>
+                <TagsComponent
+                  tagInput={tagInput}
+                  setTagInput={setTagInput}
+                  task={task}
+                  setTask={setTask}
+                  errorMessages={errorMessages}
+                  setErrorMessages={setErrorMessages}
+                />
+
                 <div className="flex justify-end">
                   <Button
                     type="button"
