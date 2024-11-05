2828d724545d6640333a333225b6653ba9d93e6a
Manikanta M <manikanta.m@labsquire.com>
2024-11-05 14:14:18 +0530

    Users Listing screen UI

--- src/components/core/TanstackTable.tsx
+++ src/components/core/TanstackTable.tsx
@@ -102,7 +102,7 @@ const TanStackTable: FC<pageProps> = ({
   return (
     <div className="overflow-x-auto w-full">
       <div
-        className={`overflow-y-auto  w-full overflow-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200 relative  bg-white rounded-[12px] ${location.pathname.includes("tasks") ? "h-[calc(100vh-320px)]" : "h-[calc(100vh-280px)]"}`}
+        className={`overflow-y-auto  w-full overflow-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200 relative  bg-white rounded-[12px] ${location.pathname.includes("tasks") ? "h-[calc(100vh-320px)]" : "h-[calc(100vh-240px)]"}`}
       >
         <Table>
           <TableHeader className="sticky top-[0px] z-10">
