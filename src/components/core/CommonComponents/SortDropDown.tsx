fc2769f27ca380a48883a9b96467f692157e50de
jawahar <jawahar.sk@labsquire.com>
2024-11-19 00:56:14 +0530

    updated few UI

--- src/components/core/CommonComponents/SortDropDown.tsx
+++ src/components/core/CommonComponents/SortDropDown.tsx
@@ -6,7 +6,7 @@ import {
 } from "@/components/ui/popover"; // Adjust the import based on your setup
 import { Button } from "@/components/ui/button"; // Adjust the import based on your setup
 import { Input } from "@/components/ui/input"; // Adjust the import based on your setup
-import { X, ChevronUp, ChevronDown } from "lucide-react"; // Adjust the icons as needed
+import { X, ChevronUp, ChevronDown,ChevronsUpDown  } from "lucide-react"; // Adjust the icons as needed
 
 const sortOptions = [
   { label: "Title Asc", value: "title:asc" },
@@ -42,7 +42,7 @@ const SortPopover = ({ selectedSort, setSelectedSort }: any) => {
   return (
     <Popover open={sortPopoverOpen} onOpenChange={setSortPopoverOpen}>
       <PopoverTrigger asChild>
-        <Button
+        {/* <Button
           variant="outline"
           role="combobox"
           aria-expanded={sortPopoverOpen}
@@ -51,20 +51,44 @@ const SortPopover = ({ selectedSort, setSelectedSort }: any) => {
           {isSortSelected
             ? sortOptions.find((option) => option.value === selectedSort)?.label
             : "Select Sort"}
+            
           <div className="flex">
             {isSortSelected && (
               <X
                 className="mr-2 h-4 w-4 shrink-0 opacity-50"
                 onClick={handleRemoveSort}
               />
+           
             )}
             {sortPopoverOpen ? (
               <ChevronUp className="h-4 w-4 shrink-0 opacity-50" />
             ) : (
               <ChevronDown className="h-4 w-4 shrink-0 opacity-50" />
             )}
+           
           </div>
-        </Button>
+        </Button> */}
+        <Button
+  variant="outline"
+  role="combobox"
+  aria-expanded={sortPopoverOpen}
+  className="w-[200px] justify-between relative text-[#00000099] font-normal bg-[#ffffff] h-[35px] border-[#E2E2E2] rounded-[8px]"
+>
+  {isSortSelected
+    ? sortOptions.find((option) => option.value === selectedSort)?.label
+    : "Select Sort"}
+    
+  <div className="flex">
+    {isSortSelected && (
+      <X
+        className="mr-2 h-4 w-4 shrink-0 opacity-50"
+        onClick={handleRemoveSort}
+      />
+    )}
+    <ChevronsUpDown className="absolute right-[2%] top-[50%] -translate-y-1/2 bg-red-700 text-white rounded-full w-[20px] h-[20px] p-1" />
+  </div>
+</Button>
+
       </PopoverTrigger>
       <PopoverContent className="w-[200px] p-0">
        
@@ -80,12 +104,15 @@ const SortPopover = ({ selectedSort, setSelectedSort }: any) => {
             <Button
               key={option.value}
               onClick={() => handleSortChange(option.value)}
-              className="w-full justify-start font-normal border-none bg-white text-[#343434] capitalize  hover:bg-[#f7f8fa] hover:text-[#343434]"
+              className="w-full grid grid-cols-[10px,auto] items-center justify-start gap-x-2 font-normal border-none bg-white text-[#343434] capitalize  hover:bg-[#f7f8fa] hover:text-[#343434]"
             >
+              <p>
               {isSortSelected && selectedSort === option.value && (
-                <span className="mr-2">✓</span> // Checkmark for selected option
+                <span>✓</span> // Checkmark for selected option
               )}
-              {option.label}
+              </p>
+            
+              <p className="!mt-0">{option.label}</p>
             </Button>
           ))}
         </div>
