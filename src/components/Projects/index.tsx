ec069993b2ef6000a531754c52c9805a1b150994
code <code@labsquire.com>
2024-12-12 17:52:52 +0530

    MA--added no data img in projects

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -231,7 +231,14 @@ const Projects = () => {
         {viewMode === "card" ? (
           <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 overflow-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200 mt-3">
             {projectsData.length === 0 && !isLoading ? (
-              <div className="col-span-full text-center">No Project found</div>
+              <div className="col-span-full text-center text-lg">
+                <img
+                  src="/No data.svg"
+                  alt="No Data"
+                  height={500}
+                  width={500}
+                />
+              </div>
             ) : (
               projectsData?.map((project: any) => (
                 <ProjectCard
