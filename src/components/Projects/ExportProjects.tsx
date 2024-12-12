46c86ff6092f0a78ab59a1293d1785fa02c9081d
code <code@labsquire.com>
2024-12-12 20:32:06 +0530

    Ma--changed in projects export

--- src/components/Projects/ExportProjects.tsx
+++ src/components/Projects/ExportProjects.tsx
@@ -13,6 +13,7 @@ export const ExportProjects = ({
 }: any) => {
   const [loading, setLoading] = useState(false);
   const [projects, setProjects] = useState<any[]>([]);
+  console.log("Projects:", projects);
 
   const queryParams = {
     status: selectedStatus,
@@ -23,7 +24,7 @@ export const ExportProjects = ({
 
   console.log("Query Params:", queryParams);
 
-  const { refetch, isLoading, isError, data } = useQuery({
+  const { isLoading, isError, data } = useQuery({
     queryKey: ["getExportProjects", selectedStatus, orderBy, search_string],
     queryFn: async () => {
       const response = await exportProjectsAPI(queryParams);
@@ -50,11 +51,35 @@ export const ExportProjects = ({
     enabled: Boolean(selectedStatus || search_string || orderBy),
   });
 
+  const getProjectsDataForExport = async () => {
+    try {
+      const response = await exportProjectsAPI(queryParams);
+      const projectData = response?.data?.data.map((project: any) => ({
+        Title: project.title || "--",
+        Description: project.description || "--",
+        Code: project.code || "--",
+        Slug: project.slug || "--",
+        Status: project.active ? "Active" : "Inactive",
+        "Created On": momentWithTimezone(project.created_at) || "--",
+        Assignees: project.assignees
+          ? project.assignees
+              .map(
+                (assignee: any) =>
+                  `${assignee.user_first_name} ${assignee.user_last_name} (${assignee.user_role})`
+              )
+              .join(", ")
+          : "--",
+      }));
+      setProjects(projectData || []);
+      return projectData;
+    } catch (error) {
+      console.error(error);
+    }
+  };
   const exportToCSV = async () => {
     setLoading(true);
     try {
-      await refetch();
-      const projectData = projects.length > 0 ? projects : [];
+      let projectData = await getProjectsDataForExport();
       if (projectData.length > 0) {
         const worksheet = XLSX.utils.json_to_sheet(projectData);
         const csvData = XLSX.utils.sheet_to_csv(worksheet);
@@ -98,7 +123,7 @@ export const ExportProjects = ({
             color: "#666",
           },
         }}
-        disabled={loading}
+        disabled={data?.length == 0}
       >
         {loading ? "Exporting..." : "Export"}
       </Button>
